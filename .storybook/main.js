2d8b0ee2f0898c6f9590022988fd419779e3f24f
Evgeny Arshinov <earshinov@nvidia.com>
2020-12-05 15:18:42 +0300

    Add a workaround for https://github.com/storybookjs/storybook/issues/13381

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,23 @@ module.exports = {
   addons: [
     '@storybook/preset-scss',
   ],
-  //webpackFinal: async (config, { configType }) => {
-  //  console.log("Webpack config")
-  //  console.dir(config, { depth: null })
-  //  return config;
-  //},
+  webpackFinal: async (config, { configType }) => {
+    //console.log("Webpack config")
+    //console.dir(config, { depth: null })
+
+    // A workaround for https://github.com/storybookjs/storybook/issues/13381
+    function checkUse(use) {
+      if (use.loader && use.loader.includes('ts-loader') && 'configFile' in use.options && use.options.configFile === undefined)
+        delete use.options.configFile;
+    }
+    for (let rule of config.module.rules) {
+      if (rule.use)
+        for (let use of rule.use)
+          checkUse(use);
+      else
+        checkUse(rule);
+    }
+
+    return config;
+  },
 };
